a99f9567da4159bbee61292402ba844a1d8d2eda
jaroondo <44073146+jaroondo@users.noreply.github.com>
2019-01-02 20:23:09 +0100

    Set up mongoose

---
+++ server/server.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodApp', {useNewUrlParser: true});
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+
+/* var newTodo = new Todo({
+    text: 'Cook a dinner'
+});
+
+newTodo.save().then((doc) => {
+    console.log('Saved todo ', doc);
+}, (err) => {
+    console.log('Unable to save todo');
+}); */
+
+var otherTodo = new Todo({
+    text: 'Feed the cat',
+    completed: true,
+    completedAt: 123
+});
+
+otherTodo.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (err) => {
+    console.log('Unable to save todo', err);
+});
