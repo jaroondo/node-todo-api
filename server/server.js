78bbc5669710c04b2375eb76a1c1ded42394e32c
jaroondo <44073146+jaroondo@users.noreply.github.com>
2019-01-09 20:48:14 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodApp', {useNewUrlParser: true});
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
@@ -25,14 +30,33 @@ newTodo.save().then((doc) => {
     console.log('Unable to save todo');
 }); */
 
-var otherTodo = new Todo({
-    text: 'Feed the cat',
-    completed: true,
-    completedAt: 123
+/* var otherTodo = new Todo({
+    text: '   Edit this video   '
 });
 
 otherTodo.save().then((doc) => {
     console.log(JSON.stringify(doc, undefined, 2));
 }, (err) => {
     console.log('Unable to save todo', err);
+}); */
+
+// cvicenie
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+var user = new User({
+    email: 'andrew@example.com'
+});
+
+user.save().then((doc) => {
+    console.log('User saved', doc);
+}, (err) => {
+    console.log('Unable to save user', err);
 });
